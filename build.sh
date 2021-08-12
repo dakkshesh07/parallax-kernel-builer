938c5134a4bdc3f17294ab11e21d40e057a7c872
Dakkshesh <dakkshesh5@gmail.com>
2021-08-12 19:42:32 +0530

    build.sh: adapt for new LTO changes in the kernel
    Signed-off-by: Dakkshesh <dakkshesh5@gmail.com>

--- build.sh
+++ build.sh
@@ -17,9 +17,26 @@ KERNELDIR=$PWD
 if [[ "$COMPILER" == "CLANG" ]]; then
   COMPILERNAME=$("$KERNELDIR"/clang/bin/clang --version | head -n 1 | sed 's|\(.*\)(.*|\1|')
   export KBUILD_COMPILER_STRING=$("$KERNELDIR"/clang/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
+
+  sed -i 's/CONFIG_LTO=y/# CONFIG_LTO is not set/g' arch/arm64/configs/RMX1921_defconfig
+  sed -i 's/# CONFIG_ARCH_SUPPORTS_LTO_CLANG is not set/CONFIG_ARCH_SUPPORTS_LTO_CLANG=y/g' arch/arm64/configs/RMX1921_defconfig
+  sed -i 's/CONFIG_LTO_NONE=y/# CONFIG_LTO_NONE is not set/g' arch/arm64/configs/RMX1921_defconfig
+  sed -i 's/# CONFIG_LTO_CLANG is not set/CONFIG_LTO_CLANG=y/g' arch/arm64/configs/RMX1921_defconfig
+  sed -i 's/CONFIG_LTO_GCC=y/# CONFIG_LTO_GCC is not set/g' arch/arm64/configs/RMX1921_defconfig
+  sed -i 's/# CONFIG_LLVM_POLLY is not set/CONFIG_LLVM_POLLY=y/g' arch/arm64/configs/RMX1921_defconfig
+  sed -i '/CONFIG_THINLTO=y/d' arch/arm64/configs/RMX1921_defconfig
+
 elif [[ "$COMPILER" == "GCC" ]]; then
   COMPILERNAME=$("$KERNELDIR"/gcc-arm64/bin/aarch64-elf-gcc --version | head -n 1 | sed "s/^[^ ]* //")
   export KBUILD_COMPILER_STRING=$("$KERNELDIR"/gcc-arm64/bin/aarch64-elf-gcc --version | head -n 1)
+
+  sed -i 's/# CONFIG_LTO is not set/CONFIG_LTO=y/g' arch/arm64/configs/RMX1921_defconfig
+  sed -i 's/CONFIG_ARCH_SUPPORTS_LTO_CLANG=y/# CONFIG_ARCH_SUPPORTS_LTO_CLANG is not set/g' arch/arm64/configs/RMX1921_defconfig
+  sed -i 's/CONFIG_LTO_NONE=y/# CONFIG_LTO_NONE is not set/g' arch/arm64/configs/RMX1921_defconfig
+  sed -i 's/CONFIG_LTO_CLANG=y/# CONFIG_LTO_CLANG is not set/g' arch/arm64/configs/RMX1921_defconfig
+  sed -i 's/# CONFIG_LTO_GCC is not set/CONFIG_LTO_GCC=y/g' arch/arm64/configs/RMX1921_defconfig
+  sed -i 's/CONFIG_LLVM_POLLY=y/# CONFIG_LLVM_POLLY is not set/g' arch/arm64/configs/RMX1921_defconfig
+  sed -i '/CONFIG_THINLTO=y/d' arch/arm64/configs/RMX1921_defconfig
 fi
 
 export ARCH=arm64
@@ -35,6 +52,7 @@ if [[ "$COMPILER" == "CLANG" ]]; then
                       CC=clang \
                       AR=llvm-ar \
                       NM=llvm-nm \
+                      LD=ld.lld \
                       STRIP=llvm-strip \
                       OBJCOPY=llvm-objcopy \
                       OBJDUMP=llvm-objdump \
@@ -42,6 +60,7 @@ if [[ "$COMPILER" == "CLANG" ]]; then
                       HOSTCC=clang \
                       HOSTCXX=clang++ \
                       HOSTAR=llvm-ar \
+                      HOSTLD=ld.lld \
                       CROSS_COMPILE=aarch64-linux-gnu- \
                       CROSS_COMPILE_ARM32=arm-linux-gnueabi-
 elif [[ "$COMPILER" == "GCC" ]]; then
@@ -51,12 +70,14 @@ elif [[ "$COMPILER" == "GCC" ]]; then
                       CC=aarch64-elf-gcc \
                       AR=aarch64-elf-ar \
                       NM=llvm-nm \
+                      LD=ld.lld \
                       STRIP=aarch64-elf-strip\
                       OBJCOPY=llvm-objcopy \
                       OBJDUMP=aarch64-elf-objdump \
                       OBJSIZE=llvm-size \
                       HOSTCXX=aarch64-elf-g++ \
                       HOSTAR=llvm-ar \
+                      HOSTLD=ld.lld \
                       CROSS_COMPILE=aarch64-elf- \
                       CROSS_COMPILE_ARM32=arm-eabi-
 fi
