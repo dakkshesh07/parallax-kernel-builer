b12716a2d4565f9e1fec5f8f1392e2d31e60dca0
Dakkshesh <dakkshesh5@gmail.com>
2021-08-13 12:06:08 +0530

    builder: swithc to neutron clang and dont build with gcc for now
    Signed-off-by: Dakkshesh <dakkshesh5@gmail.com>

--- builder.sh
+++ builder.sh
@@ -4,7 +4,7 @@ pacman -Syu --noconfirm --needed git bc inetutils zip libxml2 python3 \
                                  jre-openjdk jdk-openjdk flex bison libc++ python-pip
 
 git clone -q --depth=1 https://github.com/dakkshesh07/parallax_kernel_realme_rmx1921.git -b test $HOME/Kernel
-git clone -q --depth=1 https://github.com/kdrag0n/proton-clang $HOME/Kernel/clang
+git clone -q --depth=1 https://github.com/Neutron-Clang/neutron-toolchain $HOME/Kernel/clang
 git clone -q --depth=1 https://github.com/mvaisakh/gcc-arm64 -b  gcc-new $HOME/Kernel/gcc-arm64
 git clone -q --depth=1 https://github.com/mvaisakh/gcc-arm -b gcc-new $HOME/Kernel/gcc-arm32
 pip3 -q install telegram-send
@@ -16,9 +16,9 @@ mkdir $HOME/.config
 mv telegram-send.conf $HOME/.config/telegram-send.conf
 mv build.sh $HOME/Kernel/build.sh
 cd $HOME/Kernel
-bash $HOME/Kernel/build.sh GCC
-telegram-send "$(date): | Cleaning and Switching Toolchain |"
-rm -r out
-mkdir out
-make clean && make distclean && make mrproper
+#bash $HOME/Kernel/build.sh GCC
+#telegram-send "$(date): | Cleaning and Switching Toolchain |"
+#rm -r out
+#mkdir out
+#make clean && make distclean && make mrproper
 bash $HOME/Kernel/build.sh CLANG
