25437eb9c32cfa4cc4658d14ab60734eaf2253e4
Dakkshesh <dakkshesh5@gmail.com>
2021-08-22 21:58:42 +0530

    builder: use IST for timezone instead of UTC

--- builder.sh
+++ builder.sh
@@ -15,6 +15,7 @@ sed -i s/placeholder2/${CHAT_ID}/g telegram-send.conf
 mkdir $HOME/.config
 mv telegram-send.conf $HOME/.config/telegram-send.conf
 mv build.sh $HOME/Kernel/build.sh
+export TZ="Asia/Kolkata"
 cd $HOME/Kernel
 #bash $HOME/Kernel/build.sh GCC
 #telegram-send "$(date): | Cleaning and Switching Toolchain |"
