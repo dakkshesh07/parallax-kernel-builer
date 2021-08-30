2fda37e9c7877a03e956a702dc0143ac77f2a461
Dakkshesh <dakkshesh5@gmail.com>
2021-08-30 17:36:06 +0530

    build: adapt for new anykernel changes

--- build.sh
+++ build.sh
@@ -89,8 +89,8 @@ if [ -f out/arch/arm64/boot/Image.gz-dtb ]; then
   make clean
   cd ..
 
-  cp $PWD/out/arch/arm64/boot/Image.gz-dtb $ANYKERNEL3_DIR/image/
-  cp $PWD/out/arch/arm64/boot/dtbo.img $ANYKERNEL3_DIR/image/
+  cp $PWD/out/arch/arm64/boot/Image.gz-dtb $ANYKERNEL3_DIR/parallax-kernel/image/
+  cp $PWD/out/arch/arm64/boot/dtbo.img $ANYKERNEL3_DIR/parallax-kernel/image/
 
   cd $ANYKERNEL3_DIR/
   make zip
